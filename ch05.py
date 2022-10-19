fae8666c9d8e8948589ca90bd923879418d51e5d
Jaejun Jung <37354854+Jaejuna@users.noreply.github.com>
2022-10-19 14:50:50 +0000

    still on ch05

--- ch05.py
+++ ch05.py
@@ -107,9 +107,66 @@ model.compile(loss='binary_crossentrophy', optimizer=optimizers.RMSprop(lr=1e-4)
 
 
 ## 이미지 데이터 전처리
+from keras.preprocessing.image import ImageDataGenerator
 
+train_datagen = ImageDataGenerator(resclae=1./255)
+test_datagen = ImageDataGenerator(resclae=1./255)
+
+train_generator = train_datagen.flow_from_directoty(
+  train_dir,
+  target_size=(150, 150),
+  batch_size=20,
+  class_mode='binary')
+
+validation_generator = test_datagen.flow_from_directory(
+  validation_dir,
+  target_size=(150, 150),
+  batch_size=20,
+  class_mode='binary')
 
 ## 훈련
-
+history = model.fit_generator(
+  train_generator,
+  steps_per_epoch=100,
+  epochs=30,
+  validation_data=validation_generator,
+  validation_steps=50
+)
 
 ## 모델 저장
+model.save('cats_and_dogs_small_1.h5')
+
+# 훈련 정확도, 손실 그래프 그리기
+import matplotlib.pyplot as plt
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epoch = range(1, len(acc) + 1)
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
+
+## 데이터 증식 설정
+datagen = ImageDataGenerator(
+  rotation_range=20,
+  width_shift_range=0.1,
+  height_shift_range=0.1,
+  shear_range=0.1,
+  zoom_range=0.1,
+  horizontal_flip=True,
+  fill_mode='nearest'
+)
