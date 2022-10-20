9605a43202e46b10d4b7c74f98e0c5a14e8ca039
Jaejun Jung <37354854+Jaejuna@users.noreply.github.com>
2022-10-20 11:31:48 +0000

    done ch05

--- ch05.py
+++ ch05.py
@@ -170,3 +170,76 @@ datagen = ImageDataGenerator(
   horizontal_flip=True,
   fill_mode='nearest'
 )
+
+#데이터 시각화
+##중간층의 활성화 시각화
+from keras import models
+
+layer_outputs = [layer.output for layer in model.layers[:8]]  # 하위 8개 층의 출력을 추출한다.
+activation_model = models.Model(inputs=model.input, outputs=layer=outputs)  #입력에 대해 8개 층의 출력을 반환하는 모델을 만든다.
+activations = activation_model.predict(img_tenser)  # 층의 활성화마다 하나씩 8개의 넘파이 배열로 이루어진 리스트를 반환한다.
+
+plt.matshow(first_layer_activation[0, :, :, 19], cmap='viridis') # 첫 번째 층 활성화 중에서 20번째 채널 그리기
+plt.matshow(first_layer_activation[0, :, :, 15], cmap='viridis') # 16번째 채널, 사진을 보면서 어떻게 인코딩 됐는지 볼 수 있다.
+
+# 그래프 제목을 층의 이름으로
+layer_name = []
+for layer in model.layers[:8]:
+  layer_names.append(layer.name)
+
+images_per_row = 16
+
+for layer_name, layer_activation in zip(layer_names, activations):
+  n_features = layer_activation.shape[-1]
+
+  size = layer_activation.shape[-1]  # 특성 맵의 크기는 (1, size, size, n_features)입니다
+
+  # 활성화 채널을 위한 그리드 크기를 구합니다
+  n_cols = n_features // images_per_row
+  display_grid = np.zeros((size * n_cols, images_per_row * size))
+
+  # 각 활성화를 하나의 큰 그리드에 채웁니다
+  for col in range(n_cols):
+    for row in range(images_per_row):
+      channel_image = layer_activation[0, :, :, col * images_per_row + row]
+      # 그래프로 나타내기 좋게 특성을 처리합니다
+      channel_image -= channel_image.mean()
+      channel_image /= channel_image.std()
+      channel_image *= 64
+      channel_image += 128
+      channel_image = np.clip(channel_image, 0, 255).astype('uint8')
+      display_grid[col * size : (col + 1) * size, row * size : (row + 1) * size] = channel_image
+
+  # 그리드를 출력합니다
+  scale = 1. / size
+  plt.figure(figsize=(scale * display_grid.shape[1],
+                        scale * display_grid.shape[0]))
+  plt.title(layer_name)
+  plt.grid(False)
+  plt.imshow(display_grid, aspect='auto', cmap='viridis')
+
+plt.show()
+
+## 필터 시각화
+for layer_name in ['block1_conv1', 'block2_conv1', 'block3_conv1', 'block4_conv1']:
+    size = 64
+    margin = 5
+
+    # 결과를 담을 빈 (검은) 이미지
+    results = np.zeros((8 * size + 7 * margin, 8 * size + 7 * margin, 3), dtype='uint8')
+
+    for i in range(8):  # results 그리드의 행을 반복합니다
+        for j in range(8):  # results 그리드의 열을 반복합니다
+            # layer_name에 있는 i + (j * 8)번째 필터에 대한 패턴 생성합니다
+            filter_img = generate_pattern(layer_name, i + (j * 8), size=size)
+
+            # results 그리드의 (i, j) 번째 위치에 저장합니다
+            horizontal_start = i * size + i * margin
+            horizontal_end = horizontal_start + size
+            vertical_start = j * size + j * margin
+            vertical_end = vertical_start + size
+            results[horizontal_start: horizontal_end, vertical_start: vertical_end, :] = filter_img
+
+    # results 그리드를 그립니다
+    plt.figure(figsize=(20, 20))
+    plt.imshow(results)
